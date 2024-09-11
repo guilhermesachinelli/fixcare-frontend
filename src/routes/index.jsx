1c0aeed25657790b8a2228e1d405f78893bc9372
GuiHJLima <guilhermehenriquelima.564@gmail.com>
2024-09-11 08:03:49 -0300

    chore: add react navegation lib

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,11 +2,13 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from 'expo-status-bar';
 
 import StackRoutes from "./stack.routes";
+import DrawerRoutes from "./drawer.routes";
 
 export default function Routes() {
     return (
         <NavigationContainer>
-            <StackRoutes />
+            <DrawerRoutes />
+            {/*<StackRoutes />*/}
             <StatusBar style="auto" />
         </NavigationContainer>
     )
